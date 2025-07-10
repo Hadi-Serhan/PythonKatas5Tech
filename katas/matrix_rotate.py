8e845afed925a52c73ae25d501d12b21a3a43dca
Hadi-Serhan <hadixserhan@gmail.com>
2025-07-10 20:30:24 +0300

    Solved matrix _rotate with tests

--- katas/matrix_rotate.py
+++ katas/matrix_rotate.py
@@ -5,7 +5,28 @@ def rotate_matrix(matrix):
     Args:
         matrix: the 2D square matrix to rotate
     """
-    pass
+    # Check if the matrix is empty
+    if len(matrix) == 0:
+        return matrix
+    
+    # Matrix is square
+    row_len = len(matrix[0])
+    col_len = len(matrix[0])
+    # Initalize a temp matrix of size matrix so we can append to
+    temp_mat = [[0] * col_len for _ in range(row_len)]
+    # We iterate over the original matrix rows
+    for row in range(0, row_len):
+        # Each row "i" should now be in column "len(row) - i - 1", this basically "rotates" the matrix by 90 deg 
+        for column in range(0, col_len):
+            temp_mat[column][(row_len - row - 1)] = matrix[row][column]
+    
+    # We copy the rotated temp matrix into the original one
+    for row in range(0, row_len):
+        for column in range(0, col_len):
+            matrix[row][column] = temp_mat[row][column]
+    
+    return matrix
+        
 
 
 def print_matrix(matrix):
@@ -41,3 +62,4 @@ if __name__ == '__main__':
     # 7 4 1
     # 8 5 2
     # 9 6 3
+    
